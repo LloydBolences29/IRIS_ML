a7317732beedc0691170c9fc0c52dbc05b5424a5
Lloyd <lbolences05@gmail.com>
2025-12-12 16:46:32 +0800

    Done with the EDA part

--- iris.py
+++ iris.py
@@ -13,5 +13,42 @@ pd.set_option('display.max_colwidth', None)
 # Load the iris dataset from seaborn
 data = pd.read_csv("iris.csv")
 
-#Looking at the first 5 rows of the dataset
+#Looking at the first 10 rows of the dataset
 print(data.head(10))
+
+#Summary statistics of the dataset
+print("The summary statistics of the dataset are:")
+print(data.describe())
+
+#class counts
+print("The class counts of the dataset are:")
+print(data['species'].value_counts())
+
+#Missing Value checks
+print("Missing values in each column:")
+print(data.isnull().sum())
+
+# Visualizing the distribution of each feature
+data.hist(figsize=(10, 6), color='teal', edgecolor='grey', bins=20)
+plt.suptitle('Feature Distributions', fontsize=16)
+plt.show()
+
+plt.figure(figsize=(10, 8))
+
+# Calculate the correlation matrix (only for numeric columns)
+correlation_matrix = data.select_dtypes(include=['number']).corr()
+
+# Create the heatmap
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title("Correlation Heatmap")
+plt.show()
+
+#Class Distribution
+plt.figure(figsize=(6, 4))
+# Replace 'Species' with your actual target column name
+sns.countplot(x='species', data=data)
+plt.title("Class Distribution")
+plt.show()
+
+
+#Training of Naive Bayes and Decision Tree Classifier
